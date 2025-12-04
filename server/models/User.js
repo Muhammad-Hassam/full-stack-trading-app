3a7ef48d53b565ef9cea1f62d0aa728884df03d3
hassamshaikh <hassamshaikh94@gmail.com>
2025-12-04 13:22:47 +0500

    created errors and user model functions

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import crypto from "crypto";
-import { type } from "os";
-import { match } from "assert";
-import { timeStamp } from "console";
+
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError
+} from "../errors";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -47,7 +49,7 @@ const UserSchema = new mongoose.Schema(
       type: Number,
       default: 0
     },
-    blocked_until_password: {
+    blocked_until_pin: {
       type: Date,
       default: null
     },
@@ -60,3 +62,140 @@ const UserSchema = new mongoose.Schema(
     timeStamps: true
   }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("login_pin")) {
+    const salt = await bcrypt.genSalt(10);
+    this.login_pin = await bcrypt.hash(this.login_pin, salt);
+  }
+});
+
+UserSchema.statics.updatePIN = async function (email, newPIN) {
+  try {
+    const user = await this.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+    const isSamePIN = await bcrypt.compare(newPIN, user.login_pin);
+    if (isSamePIN) {
+      throw new BadRequestError("New PIN must be different from the old PIN");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPIN = await bcrypt.hash(newPIN, salt);
+    await this.findOneAndUpdate(
+      { email },
+      { login_pin: hashedPIN, wrong_pin_attempts: 0, blocked_until_pin: null }
+    );
+
+    return { success: true, message: "PIN updated successfully" };
+  } catch (error) {
+    throw error;
+  }
+};
+
+UserSchema.statics.updatePassword = async function (email, newPassword) {
+  try {
+    const user = await this.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+
+    const isSamePassword = await bcrypt.compare(newPassword, user.password);
+    if (isSamePassword) {
+      throw new BadRequestError(
+        "New password must be different from the old password"
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    await this.findOneAndUpdate(
+      { email },
+      {
+        password: hashedPassword,
+        wrong_password_attempts: 0,
+        blocked_until_password: null
+      }
+    );
+    return { success: true, message: "Password updated successfully" };
+  } catch (error) {
+    throw error;
+  }
+};
+
+UserSchema.methods.comparedPassword = async function (enteredPassword) {
+  if (this.blocked_until_password && this.blocked_until_password > new Date()) {
+    throw new UnauthenticatedError(
+      "Invalid Login attempts exceeded. Please try after 30 minutes."
+    );
+  }
+  const isMatch = await bcrypt.compare(enteredPassword, this.password);
+  if (!isMatch) {
+    this.wrong_password_attempts += 1;
+    if (this.wrong_password_attempts >= 3) {
+      this.blocked_until_password = new Date(Date.now() + 30 * 60 * 1000);
+      this.wrong_password_attempts = 0;
+    }
+    await this.save();
+  } else {
+    this.wrong_password_attempts = 0;
+    this.blocked_until_password = null;
+    await this.save();
+  }
+  return isMatch;
+};
+
+UserSchema.methods.comparePIN = async function comparePIN(enteredPassword) {
+  if (this.blocked_until_pin && this.blocked_until_pin > new Date()) {
+    throw new UnauthenticatedError("Limit exceeded,try after 30 minutes.");
+  }
+  const hashedPIN = this.login_pin;
+  const isMatch = await bcrypt.compare(enteredPassword, hashedPIN);
+  if (!isMatch) {
+    this.wrong_pin_attempts += 1;
+    if (this.wrong_pin_attempts >= 3) {
+      this.blocked_until_pin = new Date(Date.now() + 30 * 60 * 1000);
+      this.wrong_pin_attempts = 0;
+    }
+    await this.save();
+  } else {
+    this.wrong_pin_attempts = 0;
+    this.blocked_until_pin = null;
+    await this.save();
+  }
+
+  return isMatch;
+};
+
+UserSchema.methods.createAccessToken = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  );
+};
+
+UserSchema.methods.createRefreshToken = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  );
+};
+
+const User = mongoose.model("User", UserSchema);
+export default User;
